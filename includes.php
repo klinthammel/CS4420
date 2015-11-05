fa3a830580e6f006d1c48d942b608f762fe0f924
Matthew Bowker <matthewrbowker@me.com>
2015-11-04 20:57:29 -0700

    oops

--- includes.php
+++ includes.php
@@ -2,5 +2,5 @@
 
 require_once("var/site.php");
 
-require_once("config.php");
-if (file_exists("config.inc.php")) include_once("config.inc.php");
+require_once("var/config.php");
+if (file_exists("var/config.inc.php")) include_once("var/config.inc.php");
