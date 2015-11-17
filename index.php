94e3b3b05176c6ea52d8dba7819ba9a1808ae9c1
Klinton Hammel <klinthammel@gmail.com>
2015-11-16 23:01:53 -0700

    Added catalog.php and modified site.php and index.php

--- index.php
+++ index.php
@@ -10,6 +10,11 @@ $site->genNavbar();
 
 $site->contentBegin("Home");
 ?>
+
+This will eventually be loaded from a text file.
+It will be a short description of what we are.
+<br />
+<br />
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br /><br />
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br /><br />
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br /><br />
