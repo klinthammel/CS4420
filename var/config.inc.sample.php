defa6f5bcf056745d939acd9ea6c16cbc24a4f99
Matthew Bowker <matthewrbowker@me.com>
2015-11-23 16:50:40 -0700

    Config

---
+++ var/config.inc.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS["mysql_host"] = "localhost";
+
+$GLOBALS["mysql_user"] = "root";
+
+$GLOBALS["mysql_password"] = "";
+
+$GLOBALS["mysql_database"] = "CS4420";
--- var/config.php
+++ var/config.php
@@ -1,3 +1,11 @@
 <?php
 
 $GLOBALS["version"] = "01devel1";
+
+$GLOBALS["mysql_host"] = "";
+
+$GLOBALS["mysql_user"] = "root";
+
+$GLOBALS["mysql_password"] = "";
+
+$GLOBALS["mysql_database"] = "";
