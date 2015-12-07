0b04d5346039ca01e11c74ba3d940dd1ecceddc5
Matthew Bowker <matthewrbowker@me.com>
2015-12-06 20:51:30 -0700

    Comments

--- api.php
+++ api.php
@@ -1,25 +1,39 @@
 <?php
+// Gain access to the session variables
 session_start();
+
+// Include all of our classes and configuration files
 require("includes.php");
 
 // This is the API that will handle all of the behind-the-scenes stuff for the cart.
 
+//Grab the action out of the query string
 $action = strtolower($_GET["action"]);
 
-if ($action == "add" || $action == "edit") {
+// If our action is "add"
+if ($action == "add") {
+    // Grab the item name out of the query string
     $item = strtolower($_GET["item"]);
+
+    // Grab the quantity out of the query string
     $quant = $_GET["quantity"];
 
+    // Set these values into the session variable
     $_SESSION["cart"][$item] = $quant;
 }
 else if ($action == "remove") {
+    // Grab the item name out of the query string
     $item = strtolower($_GET["item"]);
+
+    // Remove it from the session variable
     unset($_SESSION["cart"][$item]);
 }
 else if ($action == "remove_all") {
+    // If the aciton is to remove all, nuke the session and generate a new one.
     session_unset();
 }
 else {
+    // This means we don't recognize the action, so quit the file and show an error message.
     die("
 <HTML>
 <BODY>
@@ -30,7 +44,7 @@ else {
 );
 }
 
-
+// If everything worked successfully, show a done message.
 echo("
 <HTML>
 <BODY>
--- cart.php
+++ cart.php
@@ -21,16 +21,24 @@ $site -> genSearch();
 //call readText method to pull information into the page from seperate source.
 $site ->readText("cart");
 
+// If we find sections of the cart set, display the table.
+// We are using map here because sizeof() doesn't handle nested arrays
 if(isset($_SESSION["cart"]) && @max(array_map('count', $_SESSION["cart"])) > 0) {
+    // Generate the heading of the table
     echo "<table><tr><th>Product</th><th>Quantity</th></tr>";
+    //Iterate through the cart, setting the product name as $key and the quantity as $value
     foreach($_SESSION["cart"] as $key=>$value) {
+        // Echo a table row with the information and a button.
         echo "<tr><td>$key</td><td>$value <button onclick='action(\"remove\",\"{$key}\")'>- Remove</button></td></tr>";
     }
+    // Close out the table.
     echo "</table>";
 
+    // Display another API button to remove all of the products out of the cart
     echo "<button onclick='action(\"remove_all\",\"\")'>-- Remove All</button>";
 }
 else {
+    // If we don't have any items in the cart, display a nice message instead.
     echo "You have no contents in your cart.";
 }
 
